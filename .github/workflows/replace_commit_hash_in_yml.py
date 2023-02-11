78e4ead3895ac3020f70c6b78f0af9ef3a7aed28
K-Wu <11816012+K-Wu@users.noreply.github.com>
2023-02-11 03:18:29 -0600

    upd pull action

--- .github/workflows/replace_commit_hash_in_yml.py
+++ .github/workflows/replace_commit_hash_in_yml.py
@@ -1,11 +1,12 @@
 import sys
 
 if __name__=="__main__":
-    with open(".github/workflows/build-and-test.yml", "r") as f:
-        lines = f.readlines()
-    with open(".github/workflows/build-and-test.yml", "w") as f:
-        for line in lines:
-            if line.startswith("  LLVM_COMMIT:"):
-                f.write("  LLVM_COMMIT: " + sys.argv[1] + "\n")
-            else:
-                f.write(line)
+    for filename in [".github/workflows/build-and-test.yml", ".github/workflows/pull.yml"]:
+        with open(filename, "r") as f:
+            lines = f.readlines()
+        with open(filename, "w") as f:
+            for line in lines:
+                if line.startswith("  LLVM_COMMIT:"):
+                    f.write("  LLVM_COMMIT: " + sys.argv[1] + "\n")
+                else:
+                    f.write(line)
