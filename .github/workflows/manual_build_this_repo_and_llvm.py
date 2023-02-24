f7d4ae86fba36694174d2c077e07329ad4669a8d
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-02-24 01:25:55 -0600

    script: add python binding build flag and refactor

--- .github/workflows/manual_build_this_repo_and_llvm.py
+++ .github/workflows/manual_build_this_repo_and_llvm.py
@@ -3,28 +3,37 @@
 from get_commit_hash_from_yml import get_ref_from_env, ensure_executed_in_git_root
 import os
 
-def manual_clone_llvm_project(llvm_dir, ref):
+def manual_clone_llvm_project(llvm_dir, git_ref):
     # create a new folder and execute git init
     os.system("mkdir {llvm_dir}".format(llvm_dir=llvm_dir))
     os.chdir("{llvm_dir}".format(llvm_dir=llvm_dir))
     os.system("git init")
     os.system("git remote add origin https://github.com/llvm/llvm-project.git")
-    os.system("git fetch --depth 1 origin +" + ref)
+    os.system("git fetch --depth 1 origin +" + git_ref)
     os.system("git checkout FETCH_HEAD")
     # go back to the root folder
     os.chdir("..")
 
 
 
-def manual_build_llvm_project(ref):
+def manual_build_llvm_project(git_ref, enable_mlir_python_bindings):
     # TODO: add an option to choose whether to build python bindings
     # To build python bindings, add the two flags specified in the documentation to the cmake command, e.g.:
     # -DMLIR_ENABLE_BINDINGS_PYTHON=ON -DMLIR_INCLUDE_TESTS=ON -DPython3_EXECUTABLE=/home/kwu/anaconda3/envs/dev_mlir/bin/python3
     # Notice that Python3_EXECUTABLE needs to be the same as the one used in run time. Otherwise ImportError (unknown location) will be thrown.
+    # https://mlir.llvm.org/docs/Bindings/Python/#building
+
     os.system("mkdir llvm-project/build")
     os.system("mkdir llvm-project/prefix")
     os.chdir("llvm-project/build")
-    os.system("cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_TARGETS_TO_BUILD=\"host\" -DCMAKE_INSTALL_PREFIX=../prefix -DLLVM_ENABLE_PROJECTS='mlir' -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_ENABLE_OCAMLDOC=OFF -DLLVM_ENABLE_BINDINGS=OFF -DLLVM_INSTALL_UTILS=ON -DLLVM_ENABLE_LLD=ON")
+    cmake_configure_command = "cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_TARGETS_TO_BUILD=\"host\" -DCMAKE_INSTALL_PREFIX=../prefix -DLLVM_ENABLE_PROJECTS='mlir' -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_ENABLE_OCAMLDOC=OFF -DLLVM_ENABLE_BINDINGS=OFF -DLLVM_INSTALL_UTILS=ON -DLLVM_ENABLE_LLD=ON"
+    
+    if enable_mlir_python_bindings:
+        # get python3 executable path by executing which python3
+        python3_executable_path = os.popen("which python3").read().strip()
+
+        cmake_configure_command += " -DMLIR_ENABLE_BINDINGS_PYTHON=ON -DMLIR_INCLUDE_TESTS=ON -DPython3_EXECUTABLE={python3_executable_path}".format(python3_executable_path=python3_executable_path)
+    os.system(cmake_configure_command)
     os.system("cmake --build . --target install")
     os.chdir("../..")
 
@@ -49,29 +58,28 @@ def manual_release_build():
 def get_hash(llvm_dir):
     os.chdir(llvm_dir)
     # read from git rev-parse HEAD
-    ref = os.popen("git rev-parse HEAD").read().strip()
+    git_hash = os.popen("git rev-parse HEAD").read().strip()
     os.chdir("..")
-    return ref
-
+    return git_hash
 
 if __name__ == "__main__":
     ensure_executed_in_git_root()
 
-    ref = get_ref_from_env()
-    # skip if 1) the folder exists and 2) the ref is the same
+    git_ref = get_ref_from_env()
+    # skip if 1) the folder exists and 2) the git_ref is the same
     if os.path.exists("llvm-project"):
         # clone the llvm-project again to check if the two hash match
-        manual_clone_llvm_project("llvm-project2",ref)
+        manual_clone_llvm_project("llvm-project2",git_ref)
         if get_hash("llvm-project") == get_hash("llvm-project2"):
-            print("The ref is the same. Skip.")
+            print("The git_ref is the same. Skip.")
             os.system("rm -rf llvm-project2")
         else:
-            print("The ref is different. Re-clone.")
-            print("llvm-project ref is " + get_hash("llvm-project"))
-            print("llvm-project ref spec is " + get_hash("llvm-project2"))
+            print("The git_ref is different. Re-clone.")
+            print("llvm-project git_ref is " + get_hash("llvm-project"))
+            print("llvm-project git_ref spec is " + get_hash("llvm-project2"))
             os.system("rm -rf llvm-project")
             os.system("mv llvm-project2 llvm-project")
     else:
-        manual_clone_llvm_project("llvm-project", ref)
-    manual_build_llvm_project(ref)
+        manual_clone_llvm_project("llvm-project", git_ref)
+    manual_build_llvm_project(git_ref, enable_mlir_python_bindings=False)
     manual_release_build()
